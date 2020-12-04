df9b4f41dcd4e20db5fe6b42f20b087f6fe3bf4d
Nasse Grön <nassegron@twitter>
2020-12-04 23:40:11 +0100

    adds latest winner code. fixes bug in prefilled winner code.

---
+++ occasions-1.0.3.js
@@ -0,0 +1,13 @@
+// "pids" are the unique numbers representing the season tickets.
+// Each season ticket has a unique pid number.
+import pids0 from "./pids-1.0.0.js";
+import pids1 from "./pids-1.0.1.js";
+import pids2 from "./pids-1.0.2.js";
+
+const occasions = [
+  { name: "27/10 - Hammarby-Häcken", quantity: 280, winningCode: "XQÄHDL", pids: pids0 },
+  { name: "29/10 - Tilläggslottning Hammarby-Häcken", quantity: 20, pids: pids1 },
+  { name: "Julkalender utlottning #1", quantity: 5, winningCode: "SNKHWD", pids: pids2 }
+];
+
+export default occasions;
